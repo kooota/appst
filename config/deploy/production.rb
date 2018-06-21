36e9afa4899335a8645ca1dd940f86a9f2c419de
kooootakp <kooota.karasawa@gmail.com>
2018-06-21 13:42:22 +0900

    recorrect ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -67,7 +67,7 @@ role :app, %w{root@160.16.144.160}
 role :web, %w{root@160.16.144.160}
 role :db,  %w{root@160.16.144.160}
 
-server ' mydomain.com',
+server ' 160.16.144.160',
 user: 'root',
 roles: %w{web app db},
 ssh_options: {
