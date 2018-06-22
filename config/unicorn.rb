14e2f35427d2f62b9642b7e432c3d14b5faf1024
kooootakp <kooota.karasawa@gmail.com>
2018-06-22 15:04:55 +0900

    change path_to_socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,8 +2,12 @@
 
 worker_processes 2
 
-listen  '/var/www/app/appst/current/socks/unicorn.sock'
-pid     '/var/www/app/appst/current/socks/unicorn.pid'
+# listen  '/var/www/app/appst/current/socks/unicorn.sock'
+# pid     '/var/www/app/appst/current/socks/unicorn.pid'
+
+listen  '/var/run/socks/unicorn.sock'
+pid     '/var/run/socks/unicorn.pid'
+
 
 log = Dir.pwd + '/log/unicorn.log'
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
