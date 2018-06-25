93083d3cb19707aa281e10b9b295670c1c9c5228
kooootakp <kooota.karasawa@gmail.com>
2018-06-25 17:17:31 +0900

    correct database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  mount_uploader :image, ImageUploader
-
   has_many :posts, through: :likes
   has_many :likes
   has_many :like_posts, through: :likes, source: :post
   has_many :comments
+
+  mount_uploader :image, ImageUploader
+
+ # crop用の仮想attribute
+  attr_accessor :image_x
+  attr_accessor :image_y
+  attr_accessor :image_w
+  attr_accessor :image_h
 
 end
