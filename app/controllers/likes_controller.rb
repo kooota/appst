a836347809947e863aee8fd815d79e78eb0045cb
kooootakp <kooota.karasawa@gmail.com>
2018-06-25 11:02:13 +0900

    ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,25 @@
 class LikesController < ApplicationController
 
-  before_action :set_post
+  # before_action :set_post
 
   def create
     @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
-    # @likes = Like.where(post_id: params[:post_id])
-    # @post = Like.find(post_id: params[:post_id])
-    # @posts = Post.all
+    @likes = Like.where(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    @posts = Post.all
 
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     like.destroy
-    # # @likes = Like.where(post_id: params[:post_id])
-    # # @post = Post.all
+    @likes = Like.where(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    @posts = Post.all
   end
 
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
+  # def set_post
+  #   @post = Post.find(params[:post_id])
+  # end
 
 end
