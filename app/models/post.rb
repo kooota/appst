49c7d0fa668fb7e38f4711ae52eecfed3f879203
kooootakp <kooota.karasawa@gmail.com>
2018-07-24 16:35:47 +0900

    add side ad

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,12 @@ class Post < ActiveRecord::Base
   def like_user(user_id)
    likes.find_by(user_id: user_id)
   end
+
+  def self.search(search)
+    if search
+      Post.where(['title LIKE ?', "%#{search}%"])
+    else
+      Post.all
+    end
+  end
 end
