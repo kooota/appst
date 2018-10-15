1caaf857bf9efc317d20ef51bfc2a98ee74e5df0
kooootakp <kooota.karasawa@gmail.com>
2018-10-15 18:51:54 +0900

    add twitter_card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def get_twitter_card_info(post)
+    twitter_card = {}
+    if post
+      twitter_card[:url] = request.url
+      twitter_card[:title] = post.title
+      twitter_card[:description] = post.subtitle
+      twitter_card[:image] = post.image
+    else
+      twitter_card[:url] = 'https://appstimes.jp'
+      twitter_card[:title] = 'AppsTimes'
+      twitter_card[:description] = '最新・人気のWEBサービス・プロダクトが集まるメディア'
+      twitter_card[:image] = '/uploads/post/image/14/appstimes.jpg'
+    end
+    twitter_card[:card] = 'summary_large_image'
+    twitter_card[:site] = '@AppsTimes'
+    twitter_card
+  end
 end
