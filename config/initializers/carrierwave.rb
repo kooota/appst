c662b16e95ebf62e1a8cdff0da73d26a5100ddf2
kooootakp <kooota.karasawa@gmail.com>
2018-12-06 16:08:16 +0900

    add image

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,25 @@
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
+
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_credentials = {
+#     provider: 'AWS' ,
+#     aws_access_key_id: ENV['ACCESS_KEY_ID'] ,
+#     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'] ,
+#     region: 'ap-northeast-1' ,
+#     s3_host_name: 's3-ap-northeast-1.amazonaws.com'
+#   }
+
+#   config.fog_public     = true
+
+#   case Rails.env
+#     when 'development'
+#       config.fog_directory = 'appstimes-image'
+#       config.asset_host = 'https://s3.amazonaws.com/appstimes-image'
+#     when 'production'
+#       config.fog_directory = 'appstimes-image'
+#       config.asset_host = 'https://s3.amazonaws.com/appstimes-image'
+#     end
+# end
