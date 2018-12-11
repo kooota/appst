1abae6da043385627dc79b8a6b9db910f991d16e
kooootakp <kooota.karasawa@gmail.com>
2018-12-11 11:07:13 +0900

    admit fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,14 +5,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  storage :fog
 
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  # if Rails.env.development?
+  #   storage :file
+  # elsif Rails.env.test?
+  #   storage :file
+  # else
+  #   storage :fog
+  # end
 
 
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,6 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV['ACCESS_KEY_ID'] ,
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'] ,
     region: 'ap-northeast-1' ,
-    s3_host_name: 's3-ap-northeast-1.amazonaws.com',
   }
 
   config.fog_public     = true
