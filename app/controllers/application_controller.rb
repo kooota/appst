4b124a95c5d812050606fd4fb829141e21b7aa7e
kooootakp <kooota.karasawa@gmail.com>
2021-04-27 14:03:36 +0900

    fix: application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,11 +33,11 @@ class ApplicationController < ActionController::Base
     notifier.ping(message)
   end
 
-  unless Rails.env.development?
-    rescue_from Exception,                        with: :_render_500
-    rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
-    rescue_from ActionController::RoutingError,   with: :_render_404
-  end
+  # unless Rails.env.development?
+  #   rescue_from Exception,                        with: :_render_500
+  #   rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
+  #   rescue_from ActionController::RoutingError,   with: :_render_404
+  # end
 
   def routing_error
     raise ActionController::RoutingError, params[:path]
